45f674d5aa2d81683d250ab7be96578ead1843dc
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-18 11:27:26 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
+    <meta http-equiv="Access-Control-Allow-Origin" content="https://www.httnews.com">
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
