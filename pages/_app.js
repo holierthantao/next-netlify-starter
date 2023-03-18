cc9c3f753ba51b9f5e13442147c8cf49939a16e1
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-18 12:09:07 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
 import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
-  return (
-    <html>
-      <head>
-        <meta http-equiv="Access-Control-Allow-Origin" content="https://www.httnews.com/generator" />
-      </head>
-      <body>
-        <Component {...pageProps} />
-      </body>
-    </html>
-  )
+  return <Component {...pageProps} />
 }
 
 export default Application
