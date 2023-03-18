0d07593d597950b206ccef709ffdf86e399d4dff
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-18 13:32:03 -0700

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
 console.log('config.js loaded');
+window.OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
+console.log('defined next public api key');
 const apiKey = window.env.OPENAI_API_KEY;
 console.log(apiKey);
