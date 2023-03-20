ae46c914ec0fbab3cede925c51572b997961253a
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-19 23:04:23 -0700

    Update better.js

--- pages/api/better.js
+++ pages/api/better.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     // Handle preflight requests
     corsHandler(req, res);
+    res.status(200).end();
     return;
   }
    console.log("Handler function called!"); 
