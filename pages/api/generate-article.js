a3853a9905a54156f1fa071d701d00305cccfe66
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-19 21:37:10 -0700

    Update generate-article.js

--- pages/api/generate-article.js
+++ pages/api/generate-article.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   // Set the Access-Control-Allow-Origin header to allow requests from httnews.com
   res.setHeader('Access-Control-Allow-Origin', 'https://www.httnews.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
   res.status(200).end();
   return;
 }
