d83440d3269bfdab88f3be966a5c12a458698557
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-19 22:25:07 -0700

    Update test.js

--- pages/api/test.js
+++ pages/api/test.js
@@ -9,7 +9,7 @@ const corsHandler = cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     // Handle preflight requests
     corsHandler(req, res);
