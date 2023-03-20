7bcfb34f65c31f30c6ec99a89a8fdb1ca5552284
holierthantao <128203624+holierthantao@users.noreply.github.com>
2023-03-19 18:52:17 -0700

    Update generate-article.js

--- pages/api/generate-article.js
+++ pages/api/generate-article.js
@@ -11,7 +11,7 @@ const corsOptions = {
 
 const corsMiddleware = cors(corsOptions);
 
-export default async function handler(req, res)  {
+export default async function handler(req: NextApiRequest, res: NextApiResponse)   {
   await corsMiddleware(req, res);
   
   console.log("Handler function called!"); 
